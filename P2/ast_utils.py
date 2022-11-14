184c9b7e367b679bdeb271b95c7976390a949b48
Ulysses <ulysses.deaguilar@estudiante.uam.es>
2022-11-14 13:53:33 +0100

    Ahora están las transiciones

--- P2/ast_utils.py
+++ P2/ast_utils.py
@@ -34,22 +34,26 @@ class ASTDotVisitor(NodeVisitor):
         self.state_count = 0
 
     def generic_visit(self, node: AST) -> Any:
-        string = self.dot_state(node)
+        string,node_id= self.dot_state(node)
         interior = ""
         for field,value in iter_fields(node):
             if isinstance(value,list):
                 for item in value:
-                    self.visit(item)
+                    s_id = self.visit(item)
+                    print(node_id+ " -> " + s_id + "[label=\"%s\"]" % (field))
             elif isinstance(value,AST):
-                self.visit(value)
+                s_id = self.visit(value)
+                print(node_id+ " -> " + s_id + "[label=\"%s\"]" % (field))
             else:
                 interior += "\"%s\"=%s " % (field,value)
         string = string % interior
         print(string)
+        return node_id
 
     
-    def dot_state(self,node:AST) -> str:
+    def dot_state(self,node:AST):
         x = str(type(node).__name__) + "(%s)"
+        node_id = "s%d" % (self.state_count)
         string = 's%d[label="%s", shape=box]' % (self.state_count, x)
         self.state_count += 1
-        return string
+        return string,node_id
