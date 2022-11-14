56e5f4b9842a44055f1b9f09eb8dddc3ef68c16a
Ulysses <ulysses.deaguilar@estudiante.uam.es>
2022-11-14 13:33:03 +0100

    Interior arreglado, quedan las transiciones

--- P2/ast_utils.py
+++ P2/ast_utils.py
@@ -14,15 +14,12 @@ class ASTNestedForCounter(NodeVisitor):
         return super().visit(node)
 
     def generic_visit(self, node: AST) -> Any:
-        for field,value in iter_fields(node):
+        for _,value in iter_fields(node):
             if isinstance(value,list):
                 for item in value:
-                    if isinstance(item,ast.For):
-                        val = self.visit_For(item)
-                        if val > self._max_count:
-                            self._max_count = val
-                    elif isinstance(item,AST):
-                        self.visit(item)
+                    val = self.visit(item)
+                    if val > self._max_count:
+                        self._max_count = val
             elif isinstance(value,AST):
                 self.visit(value)
         return self._max_count
@@ -31,3 +28,28 @@ class ASTNestedForCounter(NodeVisitor):
         return 1 + self.generic_visit(node)
     
 
+class ASTDotVisitor(NodeVisitor):
+    def __init__(self) -> None:
+        super().__init__()
+        self.state_count = 0
+
+    def generic_visit(self, node: AST) -> Any:
+        string = self.dot_state(node)
+        interior = ""
+        for field,value in iter_fields(node):
+            if isinstance(value,list):
+                for item in value:
+                    self.visit(item)
+            elif isinstance(value,AST):
+                self.visit(value)
+            else:
+                interior += "\"%s\"=%s " % (field,value)
+        string = string % interior
+        print(string)
+
+    
+    def dot_state(self,node:AST) -> str:
+        x = str(type(node).__name__) + "(%s)"
+        string = 's%d[label="%s", shape=box]' % (self.state_count, x)
+        self.state_count += 1
+        return string
--- P2/example_ast.py
+++ P2/example_ast.py
@@ -1,6 +1,6 @@
 import ast
 import inspect
-from ast_utils import ASTNestedForCounter
+from ast_utils import ASTNestedForCounter,ASTDotVisitor
 
 def test_ast(hola: int, mi_lista: list):
     for i in range(0,20):
@@ -16,13 +16,19 @@ def test_ast2():
     for i in range(0,20):
         print(i)
 
-
+def print_if_pos(num):
+    if num > 0:
+        print(num)
 
 if __name__ == "__main__":
     counter = ASTNestedForCounter()
+    dot = ASTDotVisitor()
 
-    source = inspect.getsource(test_ast)
+    source = inspect.getsource(print_if_pos)
     my_ast = ast.parse(source)
+    print(dot.visit(my_ast))
+
+    print(ast.dump(my_ast, indent=2))
     print("For's anidados de test1: ", counter.visit(my_ast))
     
     source = inspect.getsource(test_ast2)
